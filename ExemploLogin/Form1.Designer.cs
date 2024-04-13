477fd6788ccf483f29fa48a6c180c0a7b0186407
davidwallacem1982 <davidwallacem@hotmail.com>
2024-04-13 00:33:39 -0300

    Add project files.

---
+++ ExemploLogin/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ExemploLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btAcessar = new System.Windows.Forms.Button();
+            this.btMinimizar = new System.Windows.Forms.PictureBox();
+            this.btFechar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btFechar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(5)))), ((int)(((byte)(10)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 330);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 56);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 222);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseDown);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Silver;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(308, 108);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(400, 27);
+            this.txtUsuario.TabIndex = 1;
+            this.txtUsuario.Text = "Usuário";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.Silver;
+            this.txtSenha.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(308, 157);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(400, 27);
+            this.txtSenha.TabIndex = 2;
+            this.txtSenha.Text = "Senha";
+            this.txtSenha.Enter += new System.EventHandler(this.txtSenha_Enter);
+            this.txtSenha.Leave += new System.EventHandler(this.txtSenha_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(462, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // btAcessar
+            // 
+            this.btAcessar.BackColor = System.Drawing.Color.LightGray;
+            this.btAcessar.FlatAppearance.BorderSize = 0;
+            this.btAcessar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btAcessar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btAcessar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAcessar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAcessar.Location = new System.Drawing.Point(308, 204);
+            this.btAcessar.Name = "btAcessar";
+            this.btAcessar.Size = new System.Drawing.Size(400, 40);
+            this.btAcessar.TabIndex = 3;
+            this.btAcessar.Text = "ACESSAR";
+            this.btAcessar.UseVisualStyleBackColor = false;
+            // 
+            // btMinimizar
+            // 
+            this.btMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btMinimizar.Image")));
+            this.btMinimizar.Location = new System.Drawing.Point(721, 0);
+            this.btMinimizar.Name = "btMinimizar";
+            this.btMinimizar.Size = new System.Drawing.Size(15, 15);
+            this.btMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btMinimizar.TabIndex = 5;
+            this.btMinimizar.TabStop = false;
+            this.btMinimizar.Click += new System.EventHandler(this.btMinimizar_Click);
+            // 
+            // btFechar
+            // 
+            this.btFechar.Image = ((System.Drawing.Image)(resources.GetObject("btFechar.Image")));
+            this.btFechar.Location = new System.Drawing.Point(742, 0);
+            this.btFechar.Name = "btFechar";
+            this.btFechar.Size = new System.Drawing.Size(15, 15);
+            this.btFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btFechar.TabIndex = 6;
+            this.btFechar.TabStop = false;
+            this.btFechar.Click += new System.EventHandler(this.btFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 330);
+            this.Controls.Add(this.btFechar);
+            this.Controls.Add(this.btMinimizar);
+            this.Controls.Add(this.btAcessar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btFechar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btAcessar;
+        private System.Windows.Forms.PictureBox btMinimizar;
+        private System.Windows.Forms.PictureBox btFechar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ ExemploLogin/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ExemploLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Controle para limpar os campos quando os mesmos estiverem com foco
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "Usuário")
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Usuário";
+            }
+        }
+
+        private void txtSenha_Enter(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "Senha")
+            {
+                txtSenha.Text = "";
+                txtSenha.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtSenha_Leave(object sender, EventArgs e)
+        {
+            if (txtSenha.Text == "")
+            {
+                txtSenha.Text = "Senha";
+                txtSenha.UseSystemPasswordChar = false;
+            }
+        }
+
+        #endregion
+
+        #region Minimizar e Fechar a aplicação
+
+        private void btFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        #endregion
+
+        #region Para pode mover a Janela pela tela
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void DragForm(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragForm(sender, e);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragForm(sender, e);
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragForm(sender, e);
+        }
+
+        #endregion       
+    }
+}
